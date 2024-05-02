05c4e9a50d758b09dbaf27c311e4bb1be62aa79e
Csingh26 <singh.chaittanya@gmail.com>
2024-05-02 11:51:10 +0530

    feature-selection

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn.feature_selection import SelectKBest, f_regression
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -25,6 +26,34 @@ corrMatrix = data.corr()
 mask = np.triu(np.ones_like(corrMatrix, dtype=bool))
 
 plt.figure(figsize=(20,15))
-sns.heatmap(corrMatrix, mask=mask, cmap='coolwarm', annot=True, linewidths=2, fmt = ".2f")
+sns.heatmap(corrMatrix, mask=mask, cmap=sns.diverging_palette(220, 10, as_cmap=True), 
+            annot=True, linewidths=2, fmt = ".2f", square=True)
 plt.title("Coorelation Heatmap")
 plt.show()
+
+#Feature Selection
+
+##Univariate Feature Selection
+X = data.drop(['diagnosis'], axis=1)
+y = data['diagnosis']
+
+selector = SelectKBest(f_regression, k='all')
+fit = selector.fit(X,y)
+
+pVals = pd.DataFrame(fit.pvalues_)
+scores = pd.DataFrame(fit.scores_)
+inputVars = pd.DataFrame(X.columns)
+stats = pd.concat([inputVars, pVals, scores], axis=1)
+stats.columns = ("InputVariable", "p_values", "f_scores")
+stats.sort_values(by='p_values', inplace=True)
+
+pValueThreshold = 0.05
+scoreThreshold = 5
+
+varsSelected = stats.loc[(stats['f_scores'] >= scoreThreshold) & 
+                         (stats['p_values'] <= pValueThreshold)]
+
+varsSelected = varsSelected['InputVariable'].tolist()
+XNew = X[varsSelected]
+
+print(XNew.info())
