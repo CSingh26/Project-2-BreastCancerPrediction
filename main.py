5b0d9ca4c566c0b0f731025503d02e07c06c03f2
Csingh26 <singh.chaittanya@gmail.com>
2024-05-02 10:47:08 +0530

    cleaning-and-coorelation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -13,8 +14,17 @@ print(data.info())
 
 #cleaning the dataset
 data.drop('Unnamed: 32', axis=1, inplace=True)
-print(data.info())
+data.drop('id', axis=1, inplace=True)
+data['diagnosis'] = data['diagnosis'].apply(lambda val:1 if val=='M' else 0)
 
 #checcking for missing values
 print(data.isnull().sum())
 
+#Correlation HeatMap 
+corrMatrix = data.corr()
+mask = np.triu(np.ones_like(corrMatrix, dtype=bool))
+
+plt.figure(figsize=(20,15))
+sns.heatmap(corrMatrix, mask=mask, cmap='coolwarm', annot=True, linewidths=2, fmt = ".2f")
+plt.title("Coorelation Heatmap")
+plt.show()
